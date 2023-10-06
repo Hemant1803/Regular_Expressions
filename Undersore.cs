976c9d4ed592687bccb75aef84ba05b17afafeee
Hemant1803 <hshemantsharma445@gmail.com>
2023-10-06 23:43:35 +0530

    [200~find sequences of lowercase letters joined by an

---
+++ Undersore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regular_Expression
+{
+    internal class Undersore
+    {
+        public Undersore() 
+        {
+            string pattern = @"[a-z]+(_[a-z]+)+";
+
+
+            string[] Strings = { "hemant_sharma","hello world","this_is_a_long_sequence_of_lowercase_letters" };
+
+
+            foreach (string String in Strings)
+            {
+                bool isMatch = Regex.IsMatch(String, pattern);
+                Console.WriteLine($"Input: {String}");
+                Console.WriteLine($"Match: {isMatch}");
+                Console.WriteLine();
+            }
+        }
+    
+    }
+}
